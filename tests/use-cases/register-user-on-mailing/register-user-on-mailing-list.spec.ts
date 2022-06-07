4c9ab4da80ab80c31b026cd8394c6b8cb63c0ead
rocha19 <rochaarqueologia@outlook.com>
2022-06-07 12:52:36 -0300

    feat: add user-data

--- tests/use-cases/register-user-on-mailing/register-user-on-mailing-list.spec.ts
+++ tests/use-cases/register-user-on-mailing/register-user-on-mailing-list.spec.ts
@@ -1,5 +1,16 @@
+import { UserData } from '@/modules/api/use-cases/register-user-on-mailing/user-data';
+
 describe('Register user on mailing list use case', () => {
-  test('should add user with copmplete data to mailing list', () => {
-    expect(1).toBe(1);
+  test('should add user with copmplete data to mailing list', async () => {
+    const users: UserData[] = [];
+    // const repository: UserRepository = new InMemoryUserRepository(users);
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
+    //   repository,
+    // );
+    // const name = 'any-name';
+    // const email = 'any@email.com';
+    // const response = await usecase.RegisterUserOnMailingList({ name, email });
+    // const user = repository.findUserByEmail('any@email.com');
+    // expect((await user).name).toBe('any-name');
   });
 });
