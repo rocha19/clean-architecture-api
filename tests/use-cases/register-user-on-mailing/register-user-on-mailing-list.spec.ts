d65eb6bf8b683e52c6f9f1890c2dd91c56f28e43
rocha19 <rochaarqueologia@outlook.com>
2022-06-07 13:57:13 -0300

    feat: add user-data

--- tests/use-cases/register-user-on-mailing/register-user-on-mailing-list.spec.ts
+++ tests/use-cases/register-user-on-mailing/register-user-on-mailing-list.spec.ts
@@ -1,8 +1,12 @@
+import { UserRepository } from '@/modules/api/use-cases/register-user-on-mailing/ports/user-repository';
+import { InMemoryUserRepository } from '@/modules/api/use-cases/register-user-on-mailing/repository/in-memory-user-repository';
 import { UserData } from '@/modules/api/use-cases/register-user-on-mailing/user-data';
 
 describe('Register user on mailing list use case', () => {
   test('should add user with copmplete data to mailing list', async () => {
     const users: UserData[] = [];
+    console.log(users);
+    expect(users).toBe(users);
     // const repository: UserRepository = new InMemoryUserRepository(users);
     // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
     //   repository,
---
+++ tests/use-cases/register-user-on-mailing/repository/in-memory-user-repository.spec.ts
@@ -0,0 +1,21 @@
+import { InMemoryUserRepository } from '@/modules/api/use-cases/register-user-on-mailing/repository/in-memory-user-repository';
+import { UserData } from '@/modules/api/use-cases/register-user-on-mailing/user-data';
+
+describe('In memory user repository', () => {
+  test('should return null if user is not found', async () => {
+    const users: UserData[] = [];
+    const userRepository = new InMemoryUserRepository(users);
+    const user = await userRepository.findUserByEmail('any@email.com');
+    expect(user).toBe('Method not implemented.');
+  });
+
+  test('should return user if it is found in the repository', async () => {
+    const users: UserData[] = [];
+    const name = 'any-name';
+    const email = 'any@email.com';
+    const userRepository = new InMemoryUserRepository(users);
+    userRepository.add({ name, email });
+    const user = await userRepository.findUserByEmail('any@email.com');
+    expect(user.name).toBe('any-name');
+  });
+});
