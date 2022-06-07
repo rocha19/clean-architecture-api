10c7a52efb547b12e360bd6a95068a6a0273fee0
rocha19 <rochaarqueologia@outlook.com>
2022-06-07 12:29:09 -0300

    chore: add prettier config

--- tests/use-cases/register-user-on-mailing/register-user-on-mailing-list.spec.ts
+++ tests/use-cases/register-user-on-mailing/register-user-on-mailing-list.spec.ts
@@ -1,5 +1,5 @@
-describe("Register user on mailing list use case", () => {
-  test('', () => {
-    expect(1).toBe(1)
-  })
-})
+describe('Register user on mailing list use case', () => {
+  test('should add user with copmplete data to mailing list', () => {
+    expect(1).toBe(1);
+  });
+});
