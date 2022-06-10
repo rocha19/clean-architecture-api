9a9ce96d43d21de25a1f0536da2811d0b3eeae12
rocha19 <rochaarqueologia@outlook.com>
2022-06-10 08:51:21 -0300

    feat: return status 400 for name or email

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,15 +1,14 @@
 import { UserData } from '@/entities';
 import { RegisterUserOnMailingList } from '@/use-cases/register-user-on-mailing';
 import { HttpRequest } from '@/web-controllers/ports';
-import { created } from '@/web-controllers/util';
+import { badRequest, created } from '@/web-controllers/util';
 
 export class RegisterUserController {
   constructor(private readonly usecase: RegisterUserOnMailingList) {}
   public async handle(request: HttpRequest) {
     const userData: UserData = request.body;
     const response = await this.usecase.registerUserOnMailingList(userData);
-    if (response.isRight()) {
-      return created(response.value);
-    }
+    if (response.isLeft()) return badRequest(response.value);
+    if (response.isRight()) return created(response.value);
   }
 }
