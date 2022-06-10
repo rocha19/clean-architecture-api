c440f7af78873db69b0ee183b3d777e721a59276
rocha19 <rochaarqueologia@outlook.com>
2022-06-10 09:37:44 -0300

    feat: web controller should return web server raises

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,20 +1,24 @@
 import { UserData } from '@/entities';
-import { RegisterUserOnMailingList } from '@/use-cases/register-user-on-mailing';
+import { UseCase } from '@/use-cases/ports';
 import { HttpRequest } from '@/web-controllers/ports';
-import { badRequest, created } from '@/web-controllers/util';
+import { badRequest, created, serverError } from '@/web-controllers/util';
 import { MissingParamError } from './errors/missing-param-error';
 
 export class RegisterUserController {
-  constructor(private readonly usecase: RegisterUserOnMailingList) {}
+  constructor(private readonly usecase: UseCase) {}
   public async handle(request: HttpRequest) {
-    if (!request.body.name || !request.body.email) {
-      let missingParam = !request.body.name ? 'name ' : '';
-      missingParam += !request.body.email ? 'email' : '';
-      return badRequest(new MissingParamError(missingParam.trim()));
+    try {
+      if (!request.body.name || !request.body.email) {
+        let missingParam = !request.body.name ? 'name ' : '';
+        missingParam += !request.body.email ? 'email' : '';
+        return badRequest(new MissingParamError(missingParam.trim()));
+      }
+      const userData: UserData = request.body;
+      const response = await this.usecase.perform(userData);
+      if (response.isLeft()) return badRequest(response.value);
+      if (response.isRight()) return created(response.value);
+    } catch (error) {
+      return serverError(error);
     }
-    const userData: UserData = request.body;
-    const response = await this.usecase.registerUserOnMailingList(userData);
-    if (response.isLeft()) return badRequest(response.value);
-    if (response.isRight()) return created(response.value);
   }
 }
--- src/web-controllers/util/http-helper.ts
+++ src/web-controllers/util/http-helper.ts
@@ -4,8 +4,11 @@ export const created = (data: any): HttpResponse => ({
   statusCode: 201,
   body: data,
 });
-
 export const badRequest = (data: any): HttpResponse => ({
   statusCode: 400,
+  body: data,
+});
+export const serverError = (data: any): HttpResponse => ({
+  statusCode: 500,
   body: data,
 });
