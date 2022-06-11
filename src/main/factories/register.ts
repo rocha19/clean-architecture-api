b41e48e82970c2b005a39ce5bb4112bb6c36c6b9
rocha19 <rochaarqueologia@outlook.com>
2022-06-11 11:28:48 -0300

    feat: register user controller factory

---
+++ src/main/factories/register.ts
@@ -0,0 +1,14 @@
+import { RegisterUserOnMailingList } from '@/use-cases/register-user-on-mailing';
+import { RegisterUserController } from '@/web-controllers';
+import { InMemoryUserRepository } from '@test/use-cases/register-user-on-mailing/repository';
+
+export const makeRegisterUserController = (): RegisterUserController => {
+  const inMemoryUserRepository = new InMemoryUserRepository([]);
+  const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(
+    inMemoryUserRepository,
+  );
+  const registerUserController = new RegisterUserController(
+    registerUserOnMailingListUseCase,
+  );
+  return registerUserController;
+};
