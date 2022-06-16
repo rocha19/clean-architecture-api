8acc2cf5f67bc6b3632f2c863c1a4e33c3decfd1
rocha19 <rochaarqueologia@outlook.com>
2022-06-16 10:05:00 -0300

    test: connect mongodb

--- test/main/routes/register-route.spec.ts
+++ test/main/routes/register-route.spec.ts
@@ -1,7 +1,17 @@
+import { MongoHelper } from '@/infra/repositories/mongodb/helper';
 import app from '@/main/config/app';
 import request from 'supertest';
 
 describe('Register route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(`${process.env.MONGO_URL}`);
+  });
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+  beforeEach(() => {
+    MongoHelper.clearCollection('users');
+  });
   it('should return an account on success', async () => {
     app.post('test_cors', (request, response) => {
       response.send();
