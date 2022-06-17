a1e4c7e76e973fd77aaeb81a8f72c70b5da17ed7
rocha19 <rochaarqueologia@outlook.com>
2022-06-17 09:23:12 -0300

    test: send email feature

---
+++ test/use-cases/send-email/send-email.spec.ts
@@ -0,0 +1,67 @@
+import { User } from '@/domain';
+import { Either, left, right } from '@/shared';
+import { MailServiceError } from '@/use-cases/errors';
+import { SendEmail } from '@/use-cases/send-email';
+import { EmailOptions, EmailService } from '@/use-cases/send-email/ports';
+
+const attachmentFilePath = '../resources/text.txt';
+const fromName = 'Test';
+const fromEmail = 'from_email@mail.com';
+const toName = 'any_name';
+const toEmail = 'any_email@mail.com';
+const subject = 'Test e-mail';
+const emailBody = 'Hello world attachment test';
+const emailBodyHtml = '<b>Hello world attachment test</b>';
+const attachment = [
+  {
+    filename: attachmentFilePath,
+    contentType: 'text/plain',
+  },
+];
+
+const mailOptions: EmailOptions = {
+  host: 'test',
+  port: 867,
+  username: 'test',
+  password: 'test',
+  from: fromName + ' ' + fromEmail,
+  to: toName + '<' + toEmail + '>',
+  subject: subject,
+  text: emailBody,
+  html: emailBodyHtml,
+  attachments: attachment,
+};
+
+class MailServiceStub implements EmailService {
+  async send(
+    emailOptions: EmailOptions,
+  ): Promise<Either<MailServiceError, EmailOptions>> {
+    return right(emailOptions);
+  }
+}
+
+class MailServiceErrorStub implements EmailService {
+  async send(
+    emailOptions: EmailOptions,
+  ): Promise<Either<MailServiceError, EmailOptions>> {
+    return left(new MailServiceError());
+  }
+}
+
+describe('Send email to user', () => {
+  test('should email user with valid name and email address', async () => {
+    const mailServiceStub = new MailServiceStub();
+    const useCase = new SendEmail(mailOptions, mailServiceStub);
+    const user = User.create({ name: toName, email: toEmail }).value as User;
+    const response = (await useCase.perform(user)).value as EmailOptions;
+    expect(response.to).toEqual(toName + '<' + toEmail + '>');
+  });
+
+  test('should return error when email service fails', async () => {
+    const mailServiceErrorStub = new MailServiceErrorStub();
+    const useCase = new SendEmail(mailOptions, mailServiceErrorStub);
+    const user = User.create({ name: toName, email: toEmail }).value as User;
+    const response = await useCase.perform(user);
+    expect(response.value).toBeInstanceOf(MailServiceError);
+  });
+});
